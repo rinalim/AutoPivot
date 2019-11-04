b904d86ca7702f77b2b5978e1f1514cf64b8db91
rinalim <46272802+rinalim@users.noreply.github.com>
2019-11-04 11:00:55 +0900

    Create setup.py

---
+++ AutoPivot/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys, os, time
+
+channel = input('Pin number: ')
+os.system("sudo sed -i 's/channel = .*/channel = " + channel + "/g' /opt/retropie/configs/all/AutoPivot/AutoPivot.py")
+
+if os.path.isdir('/opt/retropie/configs/all/PauseMode') == True:
+    print "PauseMode detected"
+    os.system("echo '/opt/retropie/configs/all/PauseMode/PauseMode /dev/input/js0 10 11 0 &' >> /opt/retropie/configs/all/AutoPivot/onstart.sh")
+    os.system("echo 'sudo killall pngpause' >> /opt/retropie/configs/all/AutoPivot/onend.sh")
+    os.system("echo 'sudo killall pngbg' >> /opt/retropie/configs/all/AutoPivot/onend.sh")
+    os.system("echo 'sudo killall PauseMode' >> /opt/retropie/configs/all/AutoPivot/onend.sh")
+    os.system("sudo cp /opt/retropie/configs/all/PauseMode/pause_stop.png /opt/retropie/configs/all/PauseMode/pause_stop_h.png")
+    os.system("sudo cp /opt/retropie/configs/all/PauseMode/pause_resume.png /opt/retropie/configs/all/PauseMode/pause_resume_h.png")
+    os.system('sudo convert -rotate "90" /opt/retropie/configs/all/PauseMode/pause_stop.png /opt/retropie/configs/all/PauseMode/pause_stop_v.png')
+    os.system('sudo convert -rotate "90" /opt/retropie/configs/all/PauseMode/pause_resume.png /opt/retropie/configs/all/PauseMode/pause_resume_v.png')
